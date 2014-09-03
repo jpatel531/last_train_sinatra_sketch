b00d09ea666328455f9fccd88c449ea64325cb9b
jpatel531 <jamie@notespublication.com>
2014-09-03 01:00:35 +0100

    Extracted out twilioworker into class

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'haml'
 require 'json'
 require 'twilio-ruby'
+require_relative 'twilio_worker'
 
 # Helpers
 require './lib/render_partial'
@@ -21,11 +22,7 @@ end
 post '/texts' do 
 	response['Access-Control-Allow-Origin'] = '*'
 	data = JSON.parse(request.body.read)
-	# sms_sender = Twilio::REST::Client.new("AC2ee3aaedbbcefa36241064e99199d80d", "417e9ad3fe91f49d24ebbedafee0562a")
-	# sms_sender.account.messages.create(
-	# 	from: 441295477003, 
-	# 	to: data["phone_number"],
-	# 	body: data["instructions"]
-	# 	)
-	# {status: 200}
+	TwilioWorker.new.send(data["phone_number"], data["instructions"])
+
+	{status: 200}
 end 
---
+++ twilio_worker.rb
@@ -0,0 +1,18 @@
+class TwilioWorker
+
+	attr_reader :sms_sender
+
+	def initialize
+		@sms_sender = Twilio::REST::Client.new("AC2ee3aaedbbcefa36241064e99199d80d", "417e9ad3fe91f49d24ebbedafee0562a")
+	end
+
+	def send(phone_number, instructions)
+		sms_sender.account.messages.create(
+			from: 441295477003, 
+			to: phone_number,
+			body: instructions
+		)	
+	end
+
+
+end
